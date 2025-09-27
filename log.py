e0de07fdcb7a8b354b828f9523d9ea54bda1cea5
Tatiana <nesterovich_taty@mail.ru>
2025-09-27 23:37:35 +0200

    quiz 2
    Задание из курса на степик

---
+++ log.py
@@ -0,0 +1,11 @@
+"""Интро."""
+
+# 24/09
+#
+# 1. Ознакомилась с git, VS Code
+# 2. Зарегистрировалась на GitHub, Kaggle
+# 3. Ознакомилась с платформой Kaggle
+# 4. Прошла курсы на Stepik по Kaggle и Cursor
+# 4. Изучала линтеры pylint, pydocstile, flake8, Mypy
+
+#
