75752562d7868f39db89cfbaf6a855228ecc43a2
Tatiana <nesterovich_taty@mail.ru>
2025-09-28 02:14:02 +0200

    Sync jupytext pairs (ipynb <-> py)

--- log.py
+++ log.py
@@ -1,5 +1,7 @@
+# %%
 """Интро."""
 
+# + [markdown] magic_args="[markdown]"
 # 24/09
 #
 # 1. Ознакомилась с git, VS Code
@@ -8,4 +10,5 @@
 # 4. Прошла курсы на Stepik по Kaggle и Cursor
 # 4. Изучала линтеры pylint, pydocstile, flake8, Mypy
 
+# + [markdown] magic_args="[markdown]"
 #
